afc96d15c6a4c5511b738cc52bd67e1f7031099a
pyphrb <pyphrb@163.com>
2015-09-24 15:25:24 +0800

    modify set render

--- Manager.py
+++ Manager.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 __author__ = 'pyphrb'
+import web
 from app.config import Route
 from app.config import Application
 from app.control import hello
 url = Route().url()
 app = Application(url, globals()).application()
-
+render = web.template.render('templates/', cache=False)
 
 if __name__ == "__main__":
     app.run()
